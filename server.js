b2326d172a75455fa3b0680a8640d1de663142ec
Peducator <phamsonhoangphuc0404@gmail.com>
2026-06-26 20:00:54 +0700

    login and register

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const Authroutes = require('./src/routes/auth.routes');
 
-app.listen(PORT, () => {
-    console.log('server is running at port ' + PORT);
-});
+app.use(express.json());
 
 app.use('/auth', Authroutes);
 
 app.get('/', (req,res) =>{
     res.send('hello world');
+});
+
+app.listen(PORT, () => {
+    console.log('server is running at port ' + PORT);
 });
 
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,13 +1,40 @@
-const express = require('express');
+const { PrismaClient } = require('@prisma/client')
+const bcrypt = require('bcrypt')
+const prisma = new PrismaClient()
 
-const register = (req, res) => {
-  console.log('register')
-  res.send('register')
+const register = async (req, res) => {
+  const {username, password} = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const user = await prisma.user.create({
+    data: {
+      username,
+      password: hashedPassword
+    }
+  })
+  res.json(user);
 }
 
-const login = (req, res) => {
-  console.log('login')
-  res.send('login')
+const login = async (req, res) => {
+  const {username, password} = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      username
+    }
+  })
+  if (!user){
+    res.status(401).json({message : 'invalid username or password'});
+    return;
+  }
+  else{
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+      res.status(401).json({message : 'invalid username or password'});
+      return;
+    }
+    else{
+      res.json({message : 'login successful'});
+    }
+  }
 }
 
 module.exports = { login, register }
